09da90358cec98a48d62376081ba47e4e3ea6ff6
Seif El-Din Sweilam <saifsweelam@gmail.com>
2024-01-30 17:48:26 +0200

    Implement basic functionality

--- dist/index.js
+++ dist/index.js
@@ -1,50 +1,43 @@
-import { RequestHandler, Router } from 'express';
-import { Model } from 'mongoose';
-
+"use strict";
+const express_1 = require("express");
 class Restify {
-    public static default = Restify;
-    constructor(private model: Model<any>) {}
-
-    public crud(): Router {
-        const router = Router();
-
+    model;
+    static default = Restify;
+    constructor(model) {
+        this.model = model;
+    }
+    crud() {
+        const router = (0, express_1.Router)();
         router.get('/', this.getAll);
         router.get('/:id', this.getOne);
         router.post('/', this.create);
         router.put('/:id', this.update);
         router.delete('/:id', this.delete);
-
         return router;
     }
-
-    private getAll: RequestHandler = async (req, res) => {
+    getAll = async (req, res) => {
         const data = await this.model.find();
         res.json(data);
-    }
-
-    private getOne: RequestHandler = async (req, res) => {
+    };
+    getOne = async (req, res) => {
         const { id } = req.params;
         const data = await this.model.findById(id);
         res.json(data);
-    }
-
-    private create: RequestHandler = async (req, res) => {
+    };
+    create = async (req, res) => {
         const data = await this.model.create(req.body);
         res.json(data);
-    }
-
-    private update: RequestHandler = async (req, res) => {
+    };
+    update = async (req, res) => {
         const { id } = req.params;
         const updatedData = req.body;
         const data = await this.model.findByIdAndUpdate(id, updatedData, { new: true });
         res.json(data);
-    }
-
-    private delete: RequestHandler = async (req, res) => {
+    };
+    delete = async (req, res) => {
         const { id } = req.params;
         const data = await this.model.findByIdAndDelete(id);
         res.json(data);
-    }
+    };
 }
-
-export = Restify;
+module.exports = Restify;
